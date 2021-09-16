767666ed5a125e4c8d04f5a9d1c0e9edf26e5172
Tarcísio Eduardo Moreira Crocomo <tcrocomo@esss.co>
2021-09-16 14:17:30 -0300

    Make plugin compatible with Jenkins recent div-based layout.

--- src/main/resources/org/jenkinsci/plugins/extremenotification/NotificationEndpoint/config.groovy
+++ src/main/resources/org/jenkinsci/plugins/extremenotification/NotificationEndpoint/config.groovy
@@ -3,19 +3,32 @@ package org.jenkinsci.plugins.extremenotification.NotificationEndpoint
 def f=namespace(lib.FormTagLib)
 def st=namespace("jelly:stapler")
 
+def tableOrDiv(Closure closure) {
+	if (!context.getVariableWithDefaultValue("divBasedFormLayout", false)) {
+		table(width: "100%") {
+			closure.call()
+		}
+		return;
+	}
+
+	div() {
+		closure.call()
+	}
+}
+
 st.include(it:instance, 'class':descriptor.clazz, page:"config-advanced", optional:true)
 f.advanced {
 	events = instance?.events ?: [:]
 	f.optionalBlock(name:"events", title:_("All Events"), negative:true, checked:events.isEmpty()) {
 		f.nested {
-			table(width:"100%") {
+			tableOrDiv {
 				my.ENDPOINTS.each { endpoint ->
 					f.optionalBlock(name:"event", title:endpoint, checked:events[endpoint] != null) {
 						f.invisibleEntry {
 							input(type:"hidden", name:"endpoint", value:endpoint)
 						}
 						f.nested {
-							table(width:"100%") {
+							tableOrDiv {
 								set('instance', events.get(endpoint))
 								st.include(it:instance, 'class':descriptor.clazz, page:"config-advanced-endpoint", optional:true)
 							}
